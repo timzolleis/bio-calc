b52c26a1945acf4c90f5d75203610a930ad2c31d
tim <tim@zolleis.net>
2024-02-14 13:08:40 +0100

    data validation

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,10 @@ import {Button} from "@/components/ui/button.tsx";
 import {useState} from "react";
 import {DatePicker} from "@/components/ui/date-picker.tsx";
 import {differenceInDays, format} from "date-fns";
+import {toast, Toaster} from "sonner";
 
 function App() {
     const defaultSwitchDate = localStorage.getItem("switchDate") || "2017-05-01";
-    const onSetDefaultSwitchDate = (date?: Date) => {
-        if (!date) return;
-        localStorage.setItem("switchDate", format(date, "yyyy-MM-dd"))
-        setSwitchDate(date)
-    }
-
     const [birthDate, setBirthDate] = useState<Date | undefined>();
     const [leaveDate, setLeaveDate] = useState<Date | undefined>();
     const [switchDate, setSwitchDate] = useState<Date | undefined>(new Date(defaultSwitchDate));
@@ -22,9 +17,34 @@ function App() {
     const daysLivedAfterSwitch = birthDate && leaveDate && switchDate ? birthDate.getTime() <= switchDate.getTime() ? differenceInDays(leaveDate, switchDate) : totalDaysLived : 0;
     const percentage = daysLivedAfterSwitch / totalDaysLived * 100 || 0;
 
+    const onSetBirthDate = (date?: Date) => {
+        if (!date) return;
+        if (leaveDate && date.getTime() > leaveDate.getTime()) {
+            toast.error("Das Geburtsdatum kann nicht nach dem Abgangsdatum liegen")
+            return
+        }
+        setBirthDate(date)
+    }
+
+    const onSetLeaveDate = (date?: Date) => {
+        if (!date) return;
+        if (birthDate && date.getTime() < birthDate.getTime()) {
+            toast.error("Das Abgangsdatum kann nicht vor dem Geburtsdatum liegen")
+            return
+        }
+        setLeaveDate(date)
+    }
+
+    const onSetDefaultSwitchDate = (date?: Date) => {
+        if (!date) return;
+        localStorage.setItem("switchDate", format(date, "yyyy-MM-dd"))
+        setSwitchDate(date)
+    }
+
 
     return (
         <main>
+            <Toaster richColors={true}/>
             <Navbar/>
             <div className={"w-full flex justify-center"}>
                 <PercentageCounter caption={`${daysLivedAfterSwitch} / ${totalDaysLived} Tagen`}
@@ -45,11 +65,11 @@ function App() {
                     </div>
                     <div className={"grid gap-2"}>
                         <Label>Geburtsdatum</Label>
-                        <DatePicker defaultValue={birthDate} onSelect={setBirthDate}/>
+                        <DatePicker defaultValue={birthDate} onSelect={onSetBirthDate}/>
                     </div>
                     <div className={"grid gap-2"}>
                         <Label>Abgangsdatum</Label>
-                        <DatePicker defaultValue={leaveDate} onSelect={setLeaveDate}/>
+                        <DatePicker defaultValue={leaveDate} onSelect={onSetLeaveDate}/>
                     </div>
                 </fieldset>
 
